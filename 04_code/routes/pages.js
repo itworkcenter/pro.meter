f081713eb98578baf390d5d4450f23dc82e6512a
Yang <ly.live@yahoo.com>
2016-10-25 17:49:10 -0700

    Update pages.js

--- 04_code/routes/pages.js
+++ 04_code/routes/pages.js
@@ -3,6 +3,8 @@ var express = require('express'),
     path = require('path'),
     app = express();
 
+var userController = require("../controllers/userController");
+
 // assign the swig engine to .html files
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
@@ -14,10 +16,23 @@ app.get(["/","/index.html"],function(req, res){
   })
 });
 
+app.get(["/login.html"],function(req, res){
+    pageDeal(req, res , {
+        title:"Home page"
+    })
+});
+app.get(["/login"],function(req, res){
+    userController.checkUser(req, res);
+});
+
+
 //Page deal
 function pageDeal(req, res , backJson){
-  var backStr = req.params[0]||"index";
-  res.render("index", backJson);
+var backName = req.path.match(/\w+/);
+
+var backStr = backName?backName[0]:"index";
+
+  res.render(backStr, backJson);
 }
 
 module.exports = app;
