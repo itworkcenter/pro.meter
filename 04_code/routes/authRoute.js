aa48cb8414c2f5ba1a3f412b9b077332aa82b95c
Yang <ly.live@yahoo.com>
2016-10-27 17:55:56 -0700

    Create authRoute.js

---
+++ 04_code/routes/authRoute.js
@@ -0,0 +1,44 @@
+// passprot username password authenticate
+var express = require('express'),
+    app = express(),
+    session = require("express-sessions"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local").Strategy,
+    mongoose = require('mongoose'),
+    app = express();
+
+    var userModel = require('../models/userModel.js'),
+        bcrypt = require("bcrypt");
+
+    app.use(session({secret: 'keyboard cat'}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.use(new LocalStrategy(function(name, pwd, done){
+        userModel.findOne({name: name})
+        .then(function(user){
+            if (!user) {
+               return one(null, fasle,{alert: "Incorrect username."})
+           }
+
+            if(bcrypt.compareSync(pwd, user.pwd)){
+                    return done(null, user);
+            }else{
+                    return one(null, fasle,{alert: "Incorrect password."})
+            }
+        })
+        .catch(function(err){
+            return done(err);
+        })
+
+    }))
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+             done(err, user);
+        });
+    });
