8bfc545eba892ee5766ffbe6c2e42b4244189a9a
Yang <ly.live@yahoo.com>
2016-11-02 17:15:07 -0700

    Update index.js

--- 04_code/dal/mongoose/index.js
+++ 04_code/dal/mongoose/index.js
@@ -1,6 +1,14 @@
-module.exports = {
-    uri: "mongodb://localhost:27017/meter",
-    options:{
-        server: {poolSize: 5}
-    }
-};
+var mongoose = require("mongoose");
+var mongooseCfg = require("../../configs/mongoose");
+
+var db = mongoose.createConnection(mongooseCfg.uri, mongooseCfg.options);
+
+    db.on("error", function(err){
+        if(err) throw err;
+    })
+
+    db.once("open", function callback(){
+        console.log("database connection open.");
+    })
+
+module.exports = db;
