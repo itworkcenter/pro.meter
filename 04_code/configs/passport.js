94c4d003139df31bda3229a7b1bcc996e4b50da9
Yang <ly.live@yahoo.com>
2016-11-04 17:25:56 -0700

    Update passport.js

--- 04_code/configs/passport.js
+++ 04_code/configs/passport.js
@@ -11,11 +11,13 @@ module.exports = function(passport){
 
   // used to serialize the user for the session
   passport.serializeUser(function(user, done) {
+      console.log("serializeUser");
       done(null, user.id);
   });
   // used to deserialize the user
   passport.deserializeUser(function(id, done) {
-      User.findById(id, function(err, user) {
+      console.log("deserializeUser");
+      userModel.findById(id, function(err, user) {
            done(err, user);
       });
   });
