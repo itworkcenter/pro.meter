43d238258567b9372abb21b49fef8f2a2033d701
Yang <ly.live@yahoo.com>
2016-11-06 01:15:44 -0800

    update

--- 04_code/routes/pageRoutes.js
+++ 04_code/routes/pageRoutes.js
@@ -49,7 +49,7 @@ app.get(["/admin/","/admin/index.html"], isAuthenticated, function(req, res){
 
 // upload file
 app.post('/upload', upload.array('photos', 12), function (req, res, next) {
-  console.log(req.files);
+  //console.log(req.files);
   // req.files is array of `photos` files
   // req.body will contain the text fields, if there were any
   res.send("Uploaded")
